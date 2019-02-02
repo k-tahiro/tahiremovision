62c72c163cf3f62637ba8f374e4e92e53e6077f8
k-tahiro <tahiro.k.ad@gmail.com>
2019-02-03 02:36:45 +0900

    Create Detector class

--- src/detector.py
+++ src/detector.py
@@ -7,30 +7,41 @@ import torch.nn as nn
 from torchvision import models, transforms
 
 
-def get_transformer(input_size: int):
-    return transforms.Compose([
-        transforms.Resize(input_size),
-        transforms.CenterCrop(input_size),
-        transforms.ToTensor(),
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-    ])
+class Detector:
+    def __init__(self, input_size: int, model_file: str):
+        self._set_transform(input_size)
+        self._set_model(model_file)
 
+    def _set_transform(self, input_size: int):
+        self._transform = transforms.Compose([
+            transforms.Resize(input_size),
+            transforms.CenterCrop(input_size),
+            transforms.ToTensor(),
+            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
 
-def set_parameter_requires_grad(model, feature_extracting):
-    if feature_extracting:
-        for param in model.parameters():
-            param.requires_grad = False
+    def _set_model(self, model_file: str):
+        def set_parameter_requires_grad(model, feature_extracting):
+            if feature_extracting:
+                for param in model.parameters():
+                    param.requires_grad = False
 
+        device = torch.device("cpu")
+        self._model = models.squeezenet1_0(pretrained=True)
+        set_parameter_requires_grad(self._model, True)
+        self._model.classifier[1] = nn.Conv2d(512, 2, kernel_size=(1,1), stride=(1,1))
+        self._model.num_classes = 2
+        self._model.load_state_dict(torch.load(model_file, map_location=device))
+        self._model.eval()
 
-def load_model(model_file):
-    device = torch.device("cpu")
-    model_ft = models.squeezenet1_0(pretrained=True)
-    set_parameter_requires_grad(model_ft, True)
-    model_ft.classifier[1] = nn.Conv2d(512, 2, kernel_size=(1,1), stride=(1,1))
-    model_ft.num_classes = 2
-    model_ft.load_state_dict(torch.load(model_file, map_location=device))
-    model_ft.eval()
-    return model_ft
+    def predict(self, input_file: str) -> int:
+        im = Image.open(input_file)
+        data = torch.stack([self._transform(im)])
+
+        outputs = self._model(data)
+        _, preds = torch.max(outputs, 1)
+        
+        return preds.numpy()[0]
 
 
 def parse_args():
@@ -43,16 +54,8 @@ def parse_args():
 
 def main():
     args = parse_args()
-
-    im = Image.open(args.input_file)
-    transform = get_transformer(args.input_size)
-    data = torch.stack([transform(im)])
-
-    model = load_model(args.model_file)
-    outputs = model(data)
-    _, preds = torch.max(outputs, 1)
-    
-    result = preds.numpy()[0]
+    detector = Detector(args.input_size, args.model_file)
+    result = detector.predict(args.input_file)
     print(result)
 
 
